cc1180cd64388faf007c9e1b3e2193a0f4895798
wyg <321699849@qq.com>
2022-06-03 20:59:32 +0800

    first commit

---
+++ cpp/STL/vector/normal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    vector<int> vct;
+    vct.push_back(1);
+    vct.push_back(2);
+    cout << vct[0] << " " << vct[1] << endl;
+    return 0;
+}
